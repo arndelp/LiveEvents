b0859159ba7ab5424805bf57ef9cfb5929cc8eec
arndelp <arndelp@yahoo.fr>
2025-03-16 22:41:18 +0100

    programme

--- src/Components/Programme.js
+++ src/Components/Programme.js
@@ -15,7 +15,7 @@ const Programme = ()  => {
     /*envoi une requête et récupération des données dans 'dataConcerts.json' puis les stockent dans concerts avec setConcerts*/
     useEffect( ()=>{
           const fetchItem = async () => {
-           fetch(' http://127.0.0.1:8000/api/concerts ') 
+           fetch(' https://concertslives.store/api/concerts ') 
           .then((response)=>response.json())
           .then(data=>setConcert(data.member))
           .catch(error => console.log(error))
