e2c53fcfa2bbe77c5a142d4b74e96b0b9035b111
arndelp <arndelp@yahoo.fr>
2025-05-18 22:36:30 +0200

    V2 sponsor

--- src/Components/ProgrammeDetails.js
+++ src/Components/ProgrammeDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import Card from 'react-bootstrap/Card';
 import { Button } from "flowbite-react";
 
@@ -22,44 +22,42 @@ export default function ProgrammeDetails () {
 
 
 
-  const navigate = useNavigate();
+  
   return (
     <>
-    <div >     
-      <div class= "kard pt-4 pb-4">
-    
-   
-        <Card style={{ width: '90vh' }}>
-          <div class="container">
-            <Card.Img variant="top" src={fullImageUrl} class="imageDetails mt-2" />
-          </div>
-          <Card.Body>
-            <Card.Title class="fs-1 lh-sm link-danger" >{name}</Card.Title>
-            <Card.Text class="fs-3 lh-sm">
-              {day}
-            </Card.Text>
-            <Card.Text class="fs-3 lh-sm">
-              {place}
-            </Card.Text>
-            <Card.Text class="fs-3 lh-sm">
-              {style}
-            </Card.Text>
-            <Card.Text  class="fs-4 lh-sm mt-3">
-              {details}
-            </Card.Text>
-            <Card.Text  class="fs-5 lh-sm mt-3">
-              {details2}
-            </Card.Text>
+      <div >     
+        <div class= "kard pt-4 pb-4">              
+          <Card style={{ width: '90vh' }}>
+            <div class="container">
+              <Card.Img variant="top" src={fullImageUrl} class="imageDetails mt-2" />
+            </div>
+            <Card.Body>
+              <Card.Title class="fs-1 lh-sm link-danger" >{name}</Card.Title>
+              <Card.Text class="fs-3 lh-sm">
+                {day}
+              </Card.Text>
+              <Card.Text class="fs-3 lh-sm">
+                {place}
+              </Card.Text>
+              <Card.Text class="fs-3 lh-sm">
+                {style}
+              </Card.Text>
+              <Card.Text  class="fs-4 lh-sm mt-3">
+                {details}
+              </Card.Text>
+              <Card.Text  class="fs-5 lh-sm mt-3">
+                {details2}
+              </Card.Text>
 
-      {/*Bouton Retour, utilisation de la méthode react useNavigate */}
-            <Button color="blue" onClick={() => navigate(-1)} className="boutonRetourDetail">Retour</Button>
-          </Card.Body>
-        </Card>
-    </div>
-    
-  </div>
-    
-        </>
+        {/*Bouton Retour, utilisation de la méthode react useNavigate */}
+              <Link to='/LiveEvents/Programmation'>
+                <Button color="blue"  className="boutonRetourDetail">Retour</Button>
+              </Link>
+            </Card.Body>
+          </Card>          
+        </div>      
+      </div>    
+    </>
   )
 }
 
