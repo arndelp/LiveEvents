041d35b797d6c90f86f61541da39060daf6d8a64
arndelp <arndelp@yahoo.fr>
2025-09-29 22:34:25 +0200

    V3

--- src/Pages/Registered.jsx
+++ src/Pages/Registered.jsx
@@ -8,7 +8,7 @@ function Contact() {
     <div className="contact">
         <div className="row">
             <div className="contacth1">
-                <h1>Vous êtes maintenant enregistré.</h1>
+                <h1>Vous êtes maintenant enregistré</h1>
                 <p className="textSubmitted"> Veuillez vérifier votre adresse e-mail avant de vous connecter </p>
                 <div className="submittedHome">                     
                     <Link to="/LiveEvents/Login">
