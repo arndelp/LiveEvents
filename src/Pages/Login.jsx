5db9e26b78995bf9267e4dfa0e85ff7d8199a0e6
arndelp <arndelp@yahoo.fr>
2025-09-29 15:33:40 +0200

    V3

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormLogin from "../Components/FormLogin"
+
+
+
+function Contact() {
+  
+  return (
+    <div className="contact">
+      <FormLogin />
+    </div>
+  );
+}
+export default Contact;
---
+++ src/Pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormRegister from "../Components/FormRegister"
+
+
+
+function Contact() {
+  
+  return (
+    <div className="contact">
+      <FormRegister />
+    </div>
+  );
+}
+export default Contact;
