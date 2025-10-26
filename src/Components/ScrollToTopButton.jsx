ae83f2fbab35ce039430172c3d38474102fb7440
arndelp <arndelp@yahoo.fr>
2025-10-26 16:58:00 +0100

    performances

--- src/Components/ScrollToTopButton.jsx
+++ src/Components/ScrollToTopButton.jsx
@@ -10,7 +10,7 @@ function ScrollToTopButton() {
 
   return (
     <div className={`scroll-to-top-button `}>
-      <button onClick={scrollToTop} ><div className="HautDePage"><img src={`${process.env.PUBLIC_URL}/assets/fleches-vers-le-haut.png`} id="fleche" alt="flêche"/></div><p className="HautDePage">Haut de page</p></button>
+      <button onClick={scrollToTop} ><div className="HautDePage"><img src={`${process.env.PUBLIC_URL}/assets/fleches-vers-le-haut.png`} id="fleche" alt="flêche" width={32} height={32}/></div><p className="HautDePage">Haut de page</p></button>
     </div>
   );
 
