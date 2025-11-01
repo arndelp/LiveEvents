f2af07de955d15770f00c7351812c5a7fd56f5b9
arndelp <arndelp@yahoo.fr>
2025-11-01 15:04:23 +0100

    debug

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -51,7 +51,7 @@ function Home() {
           </div>
         
           <Link to="/Programmation">
-          <div className="ConcertHomePage " width="100vw" height={400} >
+          <div className="ConcertHomePage " width={300} height={400} >
             <ConcertHomePage />
           </div>
           </Link>
